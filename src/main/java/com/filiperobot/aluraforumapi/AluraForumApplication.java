870d44a68b21d7cbce2478a2b74b2988246f990a
Filipe Lemos <filipe_lemos@live.com>
2023-05-04 18:15:39 -0300

    init commit

---
+++ src/main/java/com/filiperobot/aluraforumapi/AluraForumApplication.java
@@ -0,0 +1,13 @@
+package com.filiperobot.aluraforumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluraForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/filiperobot/aluraforumapi/AluraForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filiperobot.aluraforumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
